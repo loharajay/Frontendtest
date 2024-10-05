b711679733a183590558c7f5fe5613b56d01c33a
Lohar Ajay <103115733+Ajaylohar19@users.noreply.github.com>
2024-10-05 13:27:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//Q1 
+console.log("Q1 Start");
+const num1= 10;
+
+for( i=1 ;i<=10;i++){
+    console.log(i*10);
+} 
+
+console.log("Q2 Start");
+//q2
+ const Celsius = 12;
+
+ const Fahrenheit=(Celsius*9/5)+32 ;
+ 
+ console.log(Fahrenheit)
+
+ 
+ //q3
+ console.log("Q3 Start");
+ let Number = 20 ;
+ function check(Number){
+    if (Number>=0) {
+        consol.log("Number is Negative ");
+     }else{
+        consol.log("Number is Positive ");
+     }
+ }
+ console.log("Q3 end");
+
+ 
